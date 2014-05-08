76f89e9903f2d5a6e8171e0ecccb44a7c60a2fca
Robin <rdex@external.(none)>
2014-05-08 10:01:06 +0200

    added authentication controller

---
+++ api/controllers/AuthenticationController.js
@@ -0,0 +1,26 @@
+var passport = require('passport');
+
+module.exports = {
+	signIn: function(res, req) {
+		return res.view();
+	},
+
+	process: function(res, req) {
+		passport.authenticate('local', function(err, user, info)) {
+			if(err || !user) {
+				res.redirect('/signIn');
+				return;
+			}
+			req.logIn(user, function(err) {
+				if(err) {
+					res.redirect('/signIn');
+				}
+				return res.redirect('/');
+			});
+		}
+	},
+
+	signOut: function(res, req) {
+		req.logOut();
+	}
+}
